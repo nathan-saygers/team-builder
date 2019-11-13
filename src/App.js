4f736d204aac0bd365ab9e4b202f981a10fcd803
nathan-saygers <nathan.saygers@gmail.com>
2019-11-13 14:48:59 -0800

    basic functionality complete

--- src/App.js
+++ src/App.js
@@ -12,12 +12,21 @@ function App() {
     }
   ]);
 
+  const addNewTeamMember = member => {
+    const nextMember = {
+      id: Date.now(),
+      name: member.name,
+      position: member.position
+    };
+    setMembers([...members, nextMember])
+  }
+
   return (
     <div>
       <h1>Oh shit, there's the team</h1>
       <MemberCard members={members} />
       <h1>Oh shit, add even more team</h1>
-      <MemberForm />
+      <MemberForm addNewTeamMember={addNewTeamMember} />
     </div>
   );
 }
--- src/components/MemberForm.js
+++ src/components/MemberForm.js
@@ -2,14 +2,24 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 const MemberForm = props => {
-  const [newMember, setNewMember] = useState({name: "", postion: ""})
+  const [newMember, setNewMember] = useState({name: "", position: ""})
+
+  const handleChanges = event => {
+    setNewMember({...newMember, [event.target.name]: event.target.value})
+  }
+
+  const submitForm = event => {
+    event.preventDefault()
+    props.addNewTeamMember(newMember)
+    setNewMember({name: "", position: ""})
+  }
 
   return (
-    <form>
-      <label htmlFor="Name"></label>
-      <input type="text"/>
-      <label htmlFor="Position"></label>
-      <input type="text"/>
+    <form onSubmit={submitForm}>
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" name="name" onChange={handleChanges} value={newMember.name} />
+      <label htmlFor="position">Position</label>
+      <input type="text" id="position" name="position" onChange={handleChanges} value={newMember.position}/>
       <button type="submit">Create New Member</button>
     </form>
   )
