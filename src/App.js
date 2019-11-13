9eac082d7d34a10522f7a85568557e2e37f605bf
nathan-saygers <nathan.saygers@gmail.com>
2019-11-13 13:25:51 -0800

    created state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [members, setMembers] = useState([
+    {
+      id: 1,
+      name: "Team Member 1",
+      position: "The Main Guy"
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hello, World</h1>
     </div>
   );
 }
